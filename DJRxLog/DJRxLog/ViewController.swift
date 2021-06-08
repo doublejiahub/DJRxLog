55328945ecb5d1dd443a06637f6a0b1a5f0ea44c
haojiajia <haojiajia@zhihu.com>
2021-06-08 15:44:21 +0800

    项目创建

---
+++ DJRxLog/DJRxLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DJRxLog
+//
+//  Created by zhihu on 2021/5/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DJRxLog/Pods/Target Support Files/Pods-DJRxLog/Pods-DJRxLog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DJRxLog : NSObject
+@end
+@implementation PodsDummy_Pods_DJRxLog
+@end
